9f0f6d1f0ba1e3f3684628317b215180a59f4264
User <user@Users-MacBook-Pro.local>
2020-09-24 17:36:00 +0300

    added styles to ListAppointments component

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -2,19 +2,28 @@ import React from 'react';
 
 class ListAppointments extends React.Component{
     render(){
+        return (<div className="appointment-list item-list mb-3">
+        {this.props.appointments.map(item => (
+          <div className="pet-item col media py-3">
+            <div className="mr-3">
+              <button className="pet-delete btn btn-sm btn-danger">X</button>
+            </div>
 
-        const appts = this.props.appointments.map(item => {
-            return(
-                <div>
-                    <div>Pet: {item.petName}</div>
-                    <div>Owner: {item.ownerName}</div>
-                </div>
-            )
-        })
+            <div className="pet-info media-body">
+              <div className="pet-head d-flex">
+                <span className="pet-name">{item.petName}</span>
+                <span className="apt-date ml-auto">{item.aptDate}</span>
+              </div>
 
-        return (<div>
-            {appts}
-        </div>)
+              <div className="owner-name">
+                <span className="label-item">Owner: </span>
+                <span>{item.ownerName}</span>
+              </div>
+              <div className="apt-notes">{item.aptNotes}</div>
+            </div>
+          </div>
+        ))}
+      </div>)
     }
 }
 
